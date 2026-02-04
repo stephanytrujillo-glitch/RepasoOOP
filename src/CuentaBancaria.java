7f2f8c98d768b588d0580e929ac4895b6b282a2d
Stephany Trujillo <stephany.trujillo@estudiantesunibague.edu.co>
2026-02-04 09:35:29 -0500

    Creacion clase cuenta bancaria

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,21 @@
+public class CuentaBancaria {
+    private final String id;
+
+    private int saldo;
+
+
+    public CuentaBancaria(String id, int saldoInicial) {
+        this.id = id;
+        this.saldo = saldoInicial;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+
+}
