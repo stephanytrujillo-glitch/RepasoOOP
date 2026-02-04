e156250183f9fc6b78eef46ca8ebcea40ac5997d
Stephany Trujillo <stephany.trujillo@estudiantesunibague.edu.co>
2026-02-04 09:51:54 -0500

    metodos banco

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -18,4 +18,24 @@ public class CuentaBancaria {
     }
 
 
+    public void consignar( int valorAConsignar ) {
+        if(valorAConsignar <= 0) {
+            throw new IllegalArgumentException( "El valor no es valido");
+        }
+        this.saldo += valorAConsignar;
+    }
+
+    public void retirar(int valorARetirar){
+        if(valorARetirar <= 0 ){
+            throw new IllegalArgumentException("el valor no es valido");
+        }
+        if(valorARetirar > saldo ){
+            throw new IllegalArgumentException( "no se puede retirar de mas ");
+        }
+        this.saldo -= valorARetirar;
+    }
+
+
+
+
 }
