56c37590a3e4417a2e94899c354dada2adec2f47
Stephany Trujillo <stephany.trujillo@estudiantesunibague.edu.co>
2026-02-06 08:35:33 -0500

    Creacion plantilla notificacion

---
+++ src/Notificacion.java
@@ -0,0 +1,12 @@
+public abstract class Notificacion {
+    protected final String receptorNotificacion;
+
+    protected Notificacion(Sttring receptorNotificacion) {
+        if(receptorNotificacion = null || receptorNotificacion.isBlank()) {
+            trow new IllegalArgumentException("El receptor es invalido");
+        }
+        this.receptorNotificacion = receptorNotificacion;
+
+    }
+    public abstract String enviarMensaje(String mensaje);
+}
