745862ffb3e3318f4287fde72b31509654e071cf
Johan Soriano <johansoriano63@gmail.com>
2026-02-06 10:01:13 -0500

    Main notificacion instagram

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,15 @@ public class Main {
         cuenta1.consignar(150);
         System.out.printf("El saldo de la cuenta es: " + cuenta1.getSaldo() );
 
-        Notificacion [] notificaciones = new Notificacion[2];
+        Notificacion [] notificaciones = new Notificacion[3];
         notificaciones[0] = new NotificacionCorreo("Pepito" );
         notificaciones[1] = new NotificacionSMS("Juanito");
+        notificaciones[2] = new NotificacionInstagram("Johan");
 
         notificaciones[0].enviarMensaje("Hola mi amigo");
         notificaciones[1].enviarMensaje("Hola mi broco");
+        notificaciones[2].enviarMensaje("Te empezo a seguir Luis");
+
 
         }
     }
